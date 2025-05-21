2efebdadfc450d03717825627741330622daeb67
감자소년 <64534095+potato9j@users.noreply.github.com>
2025-05-21 11:21:14 +0900

    AUTO_SYSTEM_LOGIC2.kts 업데이트

--- WAY1/AUTO_SYSTEM_LOGIC2.kts
+++ WAY1/AUTO_SYSTEM_LOGIC2.kts
@@ -6,7 +6,7 @@ fun createTriaBalance(entrues: List<JournalEntry>): MAp<String, DOuble>{
             accountBalances[debit.accountName]=accountBalaces.
             getOrDefault(debit.accountName, 0.0) + debit.amount
         }
-        for *credit in entry.credits){
+        for (credit in entry.credits){
             accountBalancese[credit.accountName] = accountBalances.
             getOrDefault(credit.accountName, 0.0) - credit.amount
         }
